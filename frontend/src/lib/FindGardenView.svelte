57e895aab2dad291c25fbfce79135f167918b2b7
Emilie Ma <kewbish@gmail.com>
2022-10-19 11:41:03 -0700

    Finish basic directory picker view

--- frontend/src/lib/FindGardenView.svelte
+++ frontend/src/lib/FindGardenView.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
+  import { Link } from "svelte-routing";
+
+  let path = "";
 </script>
 
 <main>
-  <h3>enter a link to your local digital garden</h3>
-  <div id="input-div">
-    <input type="text" /><input type="submit" value="go!" />
+  <div id="link-main">
+    <h2>enter a link to your local digital garden</h2>
+    <div id="input-div">
+      <div id="textbox">
+        <svg
+          width="31"
+          height="25"
+          viewBox="0 0 31 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M27 4H4C1.79086 4 0 5.79086 0 8V21C0 23.2091 1.79086 25 4 25H27C29.2091 25 31 23.2091 31 21V8C31 5.79086 29.2091 4 27 4Z"
+            fill="#B3F4AD"
+          />
+          <path
+            d="M7 0H2C0.895431 0 0 0.89543 0 2V7H9V2C9 0.895431 8.10457 0 7 0Z"
+            fill="#B3F4AD"
+          />
+        </svg>
+        <input type="text" bind:value={path} />
+      </div>
+      <Link to="/calendar" {path}>
+        <input type="submit" value="go!" />
+      </Link>
+    </div>
   </div>
 </main>
 
@@ -23,27 +49,73 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
+  }
+  #link-main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     gap: 12px;
+    margin-top: -10%;
+    width: 100%;
   }
   input {
     font-family: "Source Sans Pro";
     height: 3rem;
     font-size: 18px;
-    box-shadow: 2px 4px 4px rgba(9, 186, 58, 0.25);
     border: none;
+    background: #e0ffdd;
   }
   input[type="text"] {
+    width: 100%;
+  }
+  svg {
+    padding-right: 8px;
+  }
+  #textbox {
     background: #e0ffdd;
     border-radius: 8px 0 0 8px;
     padding-left: 8px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  #textbox::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: 8px 0 0 8px;
+    padding: 1px;
+    background: linear-gradient(180deg, #bcecb4 0%, rgba(255, 255, 255, 0) 100%),
+      linear-gradient(0deg, #e0ffdd, #e0ffdd);
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+    pointer-events: none;
   }
   input[type="submit"] {
     background: #b8efb3;
     border-radius: 0 8px 8px 0;
-    padding: 8px;
+    padding: 8px 12px;
+    cursor: pointer;
+  }
+  input[type="submit"]:hover {
+    background: #88d081;
+    font-size: 20px;
   }
   #input-div {
     display: flex;
-    justify-content: center;
+    flex-direction: row;
+    border-radius: 8px;
+    box-shadow: rgba(9, 186, 58, 0.25) 2px 4px 4px;
+    width: 40%;
+  }
+  input {
+    transition: ease-in-out 0.2s;
+  }
+  input:focus {
+    outline: none;
+    box-shadow: rgba(9, 186, 58, 0.3) 4px 8px 8px;
   }
 </style>
